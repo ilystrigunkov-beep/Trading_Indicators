956a92f1b029490cddb561dcacd26f8888b7f41e
Retre1 <153209068+Retre1@users.noreply.github.com>
2026-03-13 01:32:12 +0300

    add parse_date_to_unix time

--- CsvLoader.cpp
+++ CsvLoader.cpp
@@ -46,6 +46,37 @@ namespace core {
             const unsigned doe = yoe*365+ yoe/4-yoe/100 +doy;
             return static_cast<long long>(era) * 146097 + static_cast<long long>(doe) - 719468;
         }
+        std::optional<long long> parse_date_to_unix(std::string_view text) {
+            std::string s(text);
+            while (!s.empty() && s.front()==' ')
+                s.erase(s.begin());
+
+
+            while (!s.empty() && s.back() ==' ')
+                s.pop_back();
+
+
+            if (s.size() >=2 && ((s.front()=='"' && s.back()=='"') || (s.front()=='\'' && s.back()=='\''))) {
+                s = s.substr(1,s.size()-2);
+            }
+            const auto first_sep=s.find('/');
+            const auto second_sep=s.find('/',first_sep == std::string::npos ? 0:first_sep +1);
+
+            if (first_sep == std::string::npos||second_sep == std::string::npos)
+                return std::nullopt;
+
+            try {
+                int month =std::stoi(s.substr(0,first_sep));
+                int day   =std::stoi(s.substr(first_sep+1,second_sep-first_sep-1));
+                int year  =std::stoi(s.substr(second_sep+1));
+                long long days = days_from_civil(year,month,day);
+
+                return days * 86400LL;
+            }
+            catch (...) {
+                return std::nullopt;
+            }
+        }
 
     }
 }
