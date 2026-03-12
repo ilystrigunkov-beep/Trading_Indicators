becaedd6de54ffcc187a6b74971bb85e13207ebf
ilyastrigunkov <ilystrigunkov@gmail.com>
2026-03-12 19:55:42 +0300

    New class SpikeDetector that is inherited from Analyzer class. Methods name, clone, detect, and threshold_sigma are realized.

--- SpikeDetector.h
+++ SpikeDetector.h
@@ -16,4 +16,15 @@ namespace analysis {
     using DetectionResult = std::variant<SpikeList, std::string>;
     using DetectionOutcome = std::expected<SpikeList, std::string>;
     [[nodiscard]] DetectionResult to_detection_result(DetectionOutcome outcome);
+    class SpikeDetector : public Analyzer {
+        double threshold_sigma_;
+    public:
+        explicit SpikeDetector(double threshold_sigma = 2.0) : threshold_sigma_(threshold_sigma) {}
+        [[nodiscard]] const char* name() const noexcept override { return "SpikeDetector"; }
+        [[nodiscard]] std::shared_ptr<Analyzer> clone() const override {
+            return std::make_shared<SpikeDetector>(*this);
+        }
+        [[nodiscard]] DetectionOutcome detect(std::span<const double> prices) const;
+        [[nodiscard]] double threshold_sigma() const noexcept { return threshold_sigma_; }
+    };
 }
