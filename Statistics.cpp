44f57f7f61fe38f9348dfd1b4d360eae71fe862b
ilyastrigunkov <ilystrigunkov@gmail.com>
2026-03-12 22:01:55 +0300

    Realization of function standard_deviation, that finds standard deviation from the whole data. If there is only one or fewer numbers in data, then we return 0.0, if more than 1, then firstly we will count variance and then standard deviation as a square root of the variance.

--- Statistics.cpp
+++ Statistics.cpp
@@ -1,4 +1,5 @@
 #include "Statistics.h"
+#include <cmath>
 namespace analysis {
     double mean(std::span<const double> data) {
         if (data.empty()) {
@@ -9,5 +10,17 @@ namespace analysis {
             sum += value;
         }
         return sum / static_cast<double>(data.size());
+    }
+    double standard_deviation(std::span<const double> data, double mean_value) {
+        if (data.size() <= 1) {
+            return 0.0;
+        }
+        double sum_sq = 0.0;
+        for (double v : data) {
+            const double diff = v - mean_value;
+            sum_sq += diff * diff;
+        }
+        const double variance = sum_sq / static_cast<double>(data.size() - 1);
+        return std::sqrt(variance);
     }
 }
