cd5f9744eac292c12ba6f29097ca7c960fd4f754
Retre1 <153209068+Retre1@users.noreply.github.com>
2026-03-13 02:02:24 +0300

    add custom exceptions

--- Exceptions.cpp
+++ Exceptions.cpp
@@ -1,12 +1,11 @@
-//
-// Created by Aboba on 13.03.2026.
-//
+
 
 #include "Exceptions.h"
+
 namespace core {
 
-    FileException::FileException(const std::string& message)  : std::runtime_error(message) {}
+    FileException::FileException(const std::string& message): std::runtime_error(message) {}
 
-    ParseException::ParseException(const std::string& message)  : std::runtime_error(message) {}
+    ParseException::ParseException(const std::string& message)   : std::runtime_error(message) {}
 
-}
+} // namespace core
