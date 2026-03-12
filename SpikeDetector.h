e45dad933c986be273f66091bca30ad3f5c17555
ilyastrigunkov <ilystrigunkov@gmail.com>
2026-03-12 20:47:11 +0300

    Remove DetectionResult and to_detection_result function to use std::expected directly.

--- SpikeDetector.h
+++ SpikeDetector.h
@@ -6,16 +6,13 @@
 #include <span>
 #include <string>
 #include <vector>
-#include <variant>
 namespace analysis {
     struct Spike {
         std::size_t index;
         double change_pct;
     };
     using SpikeList = std::vector<Spike>;
-    using DetectionResult = std::variant<SpikeList, std::string>;
     using DetectionOutcome = std::expected<SpikeList, std::string>;
-    [[nodiscard]] DetectionResult to_detection_result(DetectionOutcome outcome);
     class SpikeDetector : public Analyzer {
         double threshold_sigma_;
     public:
